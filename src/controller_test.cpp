30ac7abdfb4b0cffcbf9316451450cc16374ca5b
jiseongyoo-mines <39998357+jiseongyoo-mines@users.noreply.github.com>
2018-07-20 16:01:44 -0600

    test code for motor

--- src/controller_test.cpp
+++ src/controller_test.cpp
@@ -6,13 +6,29 @@
 #include <iostream>
 #include <unistd.h>
 
-//  Motor0 running test; dirA = Pin8_9, dirB = Pin8_14, enable = Pin8_28, PWM = Pin8_13
-#define MOTOR_0_PWM "P8_13"
-#define MOTOR_0_STBY "P8_8"
-#define MOTOR_0_DIRA "P8_9"
-#define MOTOR_0_DIRB "P8_14"
-#define MOTOR_0_ENABLE "P8_28"
+//  Motor0 running test; standby = Pin8_15, dirA = Pin8_9, dirB = Pin8_14, enable = Pin8_28, PWM = Pin8_13
+#define MOTOR_STBY "P8_15"
+#define MOTOR_ENABLE  "P8_28"
 
+#define MOTOR_0_PWM   "P9_14"
+#define MOTOR_0_DIR1  "P9_12"
+#define MOTOR_0_DIR2  "P9_11"
+
+#define MOTOR_1_PWM   "P9_16"
+#define MOTOR_1_DIR1  "P9_17"
+#define MOTOR_1_DIR2  "P9_18"
+
+#define MOTOR_2_PWM   "P9_19"
+#define MOTOR_2_DIR1  "P9_18"
+#define MOTOR_2_DIR2  "P9_17"
+
+#define MOTOR_3_PWM   "P8_13"
+#define MOTOR_3_DIR1  "P8_14"
+#define MOTOR_3_DIR2  "P8_9"
+
+#define TIME_ns (1)
+#define TIME_us (1000)
+#define TIME_ms (1000000)
 
 struct JointState { double joint1;};
 using namespace std;
@@ -23,7 +39,8 @@ int main(int argc, char **argv)
   char option;
   bool quite = false;
   string aux;
-  int pin;
+  int pin, motor;
+  float pwm_duty_cycle;
   GPIO::GPIOManager* gp=NULL;
   cPWM::cPWM* a;
   
@@ -33,37 +50,113 @@ int main(int argc, char **argv)
     
     switch (option){
       case 'm':
-        cout << "Hello Motor0 test" << endl;
+        cout << "Hello test" << endl;
+        cout << "Motor number (0~3): ";
+        cin >>motor;
+        cout << "PWM duty cycle (-1 ~ 1): ";
+        cin >>pwm_duty_cycle;
 
         gp = GPIO::GPIOManager::getInstance();
         // Motor standby set
-        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_STBY);
+        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_STBY);
         gp->setDirection(pin, GPIO::OUTPUT);
         gp->setValue(pin, GPIO::HIGH);
         cout << "MOTOR0 Standby : High" << endl;
-        // Motor direction set
-        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIRA);
-        gp->setDirection(pin, GPIO::OUTPUT);
-        gp->setValue(pin, GPIO::LOW);
-        cout << "MOTOR0 DirA : High" << endl;
-        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIRB);
-        gp->setDirection(pin, GPIO::OUTPUT);
-        gp->setValue(pin, GPIO::HIGH);
-        cout << "MOTOR0 DirB : Low" << endl;
         
         // Motor enable set
-        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_ENABLE);
+        pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_ENABLE);
         gp->setDirection(pin, GPIO::OUTPUT);
         gp->setValue(pin, GPIO::HIGH);
         cout << "MOTOR0 Enable : High" << endl;
         
+        switch (motor){
+          case 0:
+            // Motor0 direction set
+            aux = MOTOR_0_PWM;
+            if (pwm_duty_cycle >= 0){ // CCW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+            }else{  // CW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+            }
+            break;
+          case 1:
+            // Motor1 direction set
+            aux = MOTOR_1_PWM;
+            if (pwm_duty_cycle >= 0){ // CCW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+            }else{  // CW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+            }
+            break;
+          case 2:
+            // Motor2 direction set
+            aux = MOTOR_2_PWM;
+            if (pwm_duty_cycle >= 0){ // CCW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+            }else{  // CW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+            }
+            break;
+          case 3:
+            // Motor3 direction set
+            aux = MOTOR_3_PWM;
+            if (pwm_duty_cycle >= 0){ // CCW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+            }else{  // CW
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR1);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::HIGH);
+              pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR2);
+              gp->setDirection(pin, GPIO::OUTPUT);
+              gp->setValue(pin, GPIO::LOW);
+            }
+            break;
+          default:
+            cout <<"Wrong motor number"<<endl;
+            break;
+        }
+        
+        
         // Motor PWM set
-        aux = MOTOR_0_PWM;
-
         a = new cPWM::cPWM(aux);
 
-        a->Period(1000000000);	// 200000ns = 200us = 0.2ms = 5000hz
-        a->Duty_cycle(500000000);	// 50% duty cycle
+        a->Period(1*TIME_ms);
+        a->Duty_cycle(abs(pwm_duty_cycle)*TIME_ms);
         a->Polarity(1);
         
         cout << aux << " PWM enable" << endl;
