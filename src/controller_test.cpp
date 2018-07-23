1dda4861e2f37401ebbe211d51564d474335b4e4
jiseongyoo-mines <39998357+jiseongyoo-mines@users.noreply.github.com>
2018-07-23 11:34:54 -0600

    Add files via upload

--- src/controller_test.cpp
+++ src/controller_test.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 // #include <joint_trajectory_controller/hardware_interface_adapter.h>
 #include "controller_test/cPWM.h"
+#include "controller_test/eqep.h"
 #include "controller_test/GPIOConst.h"
 #include "controller_test/GPIOManager.h"
 #include <iostream>
@@ -10,25 +11,26 @@
 #define MOTOR_STBY "P8_15"
 #define MOTOR_ENABLE  "P8_28"
 
-#define MOTOR_0_PWM   "P9_14"
-#define MOTOR_0_DIR1  "P9_12"
-#define MOTOR_0_DIR2  "P9_11"
+#define MOTOR_3_PWM   "P9_14"
+#define MOTOR_3_DIR1  "P9_12"
+#define MOTOR_3_DIR2  "P9_11"
 
-#define MOTOR_1_PWM   "P9_16"
-#define MOTOR_1_DIR1  "P9_17"
-#define MOTOR_1_DIR2  "P9_18"
+#define MOTOR_2_PWM   "P9_16"
+#define MOTOR_2_DIR1  "P9_17"
+#define MOTOR_2_DIR2  "P9_18"
 
-#define MOTOR_2_PWM   "P9_19"
-#define MOTOR_2_DIR1  "P9_18"
-#define MOTOR_2_DIR2  "P9_17"
+#define MOTOR_1_PWM   "P9_19"
+#define MOTOR_1_DIR1  "P9_18"
+#define MOTOR_1_DIR2  "P9_17"
 
-#define MOTOR_3_PWM   "P8_13"
-#define MOTOR_3_DIR1  "P8_14"
-#define MOTOR_3_DIR2  "P8_9"
+#define MOTOR_0_PWM   "P8_13"
+#define MOTOR_0_DIR1  "P8_14"
+#define MOTOR_0_DIR2  "P8_9"
 
 #define TIME_ns (1)
 #define TIME_us (1000)
 #define TIME_ms (1000000)
+#define TIME_s  (1000000000)
 
 struct JointState { double joint1;};
 using namespace std;
@@ -44,6 +46,7 @@ int main(int argc, char **argv)
   GPIO::GPIOManager* gp=NULL;
   cPWM::cPWM* a;
   
+  /*
   while(!quite){
     cout << "select test option (m:Motor, p:PWM, g:GPIO, q:Quite): ";
     cin >> option;
@@ -77,16 +80,20 @@ int main(int argc, char **argv)
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR0_DIR1: Low"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR0_DIR2: High"<<endl;
             }else{  // CW
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR0_DIR1: High"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_0_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR0_DIR2: Low"<<endl;
             }
             break;
           case 1:
@@ -96,16 +103,20 @@ int main(int argc, char **argv)
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR1_DIR1: Low"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR1_DIR2: High"<<endl;
             }else{  // CW
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR1_DIR1: High"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_1_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR1_DIR2: Low"<<endl;
             }
             break;
           case 2:
@@ -115,16 +126,20 @@ int main(int argc, char **argv)
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR2_DIR1: Low"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR2_DIR2: High"<<endl;
             }else{  // CW
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR2_DIR1: High"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_2_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR2_DIR2: Low"<<endl;
             }
             break;
           case 3:
@@ -134,23 +149,26 @@ int main(int argc, char **argv)
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR3_DIR1: Low"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR3_DIR2: High"<<endl;
             }else{  // CW
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR1);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::HIGH);
+              cout <<"MOTOR3_DIR1: High"<<endl;
               pin = GPIO::GPIOConst::getInstance()->getGpioByKey(MOTOR_3_DIR2);
               gp->setDirection(pin, GPIO::OUTPUT);
               gp->setValue(pin, GPIO::LOW);
+              cout <<"MOTOR3_DIR2: Low"<<endl;
             }
             break;
           default:
             cout <<"Wrong motor number"<<endl;
             break;
         }
-        
         
         // Motor PWM set
         a = new cPWM::cPWM(aux);
@@ -161,7 +179,7 @@ int main(int argc, char **argv)
         
         cout << aux << " PWM enable" << endl;
         a->Enable();
-        usleep(60000000);
+        usleep(5*TIME_s);
         cout << aux << " PWM disable" << endl;
         a->Disable();
         
@@ -222,6 +240,22 @@ int main(int argc, char **argv)
         break;
 	  }
 	}
-	    
+  */
+  
+  string encoder = "/sys/devices/platform/ocp/48302000.epwmss/48302180.eqep";
+  eQEP eqep1(encoder, eQEP::eQEP_Mode_Absolute);
+    
+  // Set the unit time period to 100,000,000 ns, or 0.1 seconds
+  eqep1.set_period(100000000L);
+
+  // Query back the period
+  std::cout << "[eQEP " << encoder << "] Period = " << eqep1.get_period() << " ns" << std::endl;
+
+  // Read position indefintely
+  while(1)
+  {
+  std::cout << "[eQEP " << encoder << "] Position = " << eqep1.get_position() << std::endl;
+  }
+  
   return 0;
 }
